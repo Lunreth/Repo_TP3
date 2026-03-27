a164403debe8bf5ebc996ba41cf9f934cf6f12c5
Lunreth <corsecondary@gmail.com>
2026-03-27 14:16:51 -0300

    nuevos archivos

---
+++ EJ1_TP3/src/ej1_tp3/EJ1_TP3.java
@@ -0,0 +1,31 @@
+
+package ej1_tp3;
+
+
+public class EJ1_TP3 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        int contador = 1;
+        
+        while(contador <= 100){
+            System.out.println("Valor de contador = " + contador);
+            //contador = contador + 1;
+            contador++;
+        }
+        
+        do
+        {
+            System.out.println("Contador = " + contador);
+            contador++;
+        }while(contador <= 100);
+        */
+        for(int contador=1; contador <= 100; contador++){
+            System.out.println("Contador = " + contador);
+        }
+        
+    }
+    
+}
