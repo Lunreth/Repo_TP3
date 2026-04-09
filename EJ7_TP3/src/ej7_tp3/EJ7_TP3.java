ec8deed66dec090ce101ca4f1ce2acdf8819aa98
Lunreth <corsecondary@gmail.com>
2026-04-09 11:58:24 -0300

    EJ 7 Resuelto

--- EJ7_TP3/src/ej7_tp3/EJ7_TP3.java
+++ EJ7_TP3/src/ej7_tp3/EJ7_TP3.java
@@ -11,6 +11,25 @@ public class EJ7_TP3 {
         // DEFINICIÓN Y CREACIÓN DE VARIABLES
         Scanner objeto = new Scanner(System.in);
         
+        double millas = 0.0;
+        
+        do {
+            // MENSAJE INICIAL
+            System.out.println("Ingrese las millas: ");
+
+            // INPUT DEL USUARIO
+            millas = objeto.nextDouble();
+            
+            if( millas == 0){
+                break;
+            }
+            
+            // OPERACIONES
+            double km = millas*1.6093;
+            
+            System.out.println("Kilometros = " + km);
+            
+        } while( millas != 0 );
         
     }
     
